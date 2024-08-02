53a1fd81ff27d5b7744c28374279e728ff92f7ac
WekesaDaniel <danielwekesakakai@gmail.com>
2024-08-01 20:27:35 -0400

    static folder

--- mydjangoproject/mydjangoproject/settings.py
+++ mydjangoproject/mydjangoproject/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     # Uncomment or remove this line if you're not using it
-    # os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
